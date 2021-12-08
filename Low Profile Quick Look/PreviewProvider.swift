51ce81428a1029569235791c43dedc8170c776ab
Nindi Gill <nindi@ninxsoft.com>
2021-12-08 14:13:43 +1100

    Stripping Quick Look extension from export

--- Low Profile Quick Look/PreviewProvider.swift
+++ Low Profile Quick Look/PreviewProvider.swift
@@ -13,7 +13,7 @@ class PreviewProvider: QLPreviewProvider, QLPreviewingController {
     func providePreview(for request: QLFilePreviewRequest) async throws -> QLPreviewReply {
 
         let reply: QLPreviewReply = QLPreviewReply(dataOfContentType: .mobileconfig, contentSize: CGSize(width: 1_080, height: 720)) { preview in
-            let data: Data = Data("Hello world".utf8)
+            let data: Data = Data("Hello World!".utf8)
             preview.title = request.fileURL.deletingPathExtension().lastPathComponent
             return data
         }
